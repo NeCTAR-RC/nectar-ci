8f2b0d64d3af65d41ba9f861ecf2ab4def1944cd
Sam Morrison <sorrison@gmail.com>
2020-10-01 17:50:01 +1000

    wip
    Change-Id: I0dca3727c477bd2daac732d49d1889e80df100ce

---
+++ vars/testBuild.groovy
@@ -0,0 +1,12 @@
+def call(String name) {
+    git credentialsId: '4946c3a5-9f5e-4eac-9ec4-90e1e348db14', url: 'ssh://jenkins@review.rc.nectar.org.au:29418/NeCTAR-RC/nectar-images.git'
+    sh '''
+        set +x
+        echo "\033[34m========== Building ==========\033[0m"
+        echo ${name}
+    '''
+    script {
+        imageId = sh(script: 'uuidgen', returnStdout: true).trim()
+    }
+    stash includes: 'build/**', name: 'build'
+}
