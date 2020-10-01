29c831ad13b0691e319ba813d88829f1dd733f3d
Sam Morrison <sorrison@gmail.com>
2020-10-01 17:52:03 +1000

    wip
    Change-Id: I147ea0ca9aefbd297664e0e21d69d485d526f87b

--- vars/testBuild.groovy
+++ vars/testBuild.groovy
@@ -3,7 +3,7 @@ def call(String name) {
     sh '''
         set +x
         echo "\033[34m========== Building ==========\033[0m"
-        echo ${name}
+        echo '${name}'
     '''
     script {
         imageId = sh(script: 'uuidgen', returnStdout: true).trim()
