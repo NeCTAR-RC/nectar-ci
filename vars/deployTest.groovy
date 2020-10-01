0e34cec0238d379a2f43db6bedb9361fce574f16
Sam Morrison <sorrison@gmail.com>
2020-10-02 09:04:01 +1000

    wip
    Change-Id: I847fef215d277ba686b9adac16510fda74603ff4

--- vars/deployTest.groovy
+++ vars/deployTest.groovy
@@ -1,19 +1,17 @@
 def call(Map pipelineParams) {
     unstash 'build'
-    sh 'find .'
     script {
         imageId = sh(script: 'uuidgen', returnStdout: true).trim()
         imageName = readFile(file: 'build/.facts/nectar_name').trim()
     }
     sh "echo $imageId"
-    sh "echo \$imageId"
     dir('build') {
         withCredentials([usernamePassword(credentialsId: '5c8f1b5c-2739-465e-ab10-e674b3fb884a', usernameVariable: 'OS_USERNAME', passwordVariable: 'OS_PASSWORD')]) {
             sh '''
             #!/bin/bash
             set -x
             find .
-            echo "${imageId}"
+            echo "$imageId"
             '''
 
         }    
