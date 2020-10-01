7e4588eeb511aab8a2ca4cfe5fd0ff8d827e38b1
Sam Morrison <sorrison@gmail.com>
2020-10-01 18:05:10 +1000

    wip
    Change-Id: I1eb95bbc931ccb08b840e841cc88da8f49b6cf14

--- vars/testBuild.groovy
+++ vars/testBuild.groovy
@@ -1,14 +1,13 @@
 def call(Map config) {
-    node {
-        git credentialsId: '4946c3a5-9f5e-4eac-9ec4-90e1e348db14', url: 'ssh://jenkins@review.rc.nectar.org.au:29418/NeCTAR-RC/nectar-images.git'
-        sh '''
-        set +x
-        echo "\033[34m========== Building ==========\033[0m"
-        echo "Name = ${config.name}"
-        '''
-        script {
-          imageId = sh(script: 'uuidgen', returnStdout: true).trim()
-        }
-        stash includes: 'build/**', name: 'build'
+    git credentialsId: '4946c3a5-9f5e-4eac-9ec4-90e1e348db14', url: 'ssh://jenkins@review.rc.nectar.org.au:29418/NeCTAR-RC/nectar-images.git'
+    sh '''
+    set +x
+    echo "\033[34m========== Building ==========\033[0m"
+    echo "Name = ${config}"
+    echo "DONE"
+    '''
+    script {
+        imageId = sh(script: 'uuidgen', returnStdout: true).trim()
     }
+    stash includes: 'build/**', name: 'build'
 }
