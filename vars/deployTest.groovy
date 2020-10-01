b2b7698b0717ed4dea0223280e89e8b6103547a1
Sam Morrison <sorrison@gmail.com>
2020-10-01 18:26:44 +1000

    wip
    Change-Id: I982caed0e3182d3e7e3a4d6cdfd6aee9039d0153

---
+++ vars/deployTest.groovy
@@ -0,0 +1,13 @@
+def call(Map pipelineParams) {
+    unstash 'build'
+    dir('build') {
+        withCredentials([usernamePassword(credentialsId: '5c8f1b5c-2739-465e-ab10-e674b3fb884a', usernameVariable: 'OS_USERNAME', passwordVariable: 'OS_PASSWORD')]) {
+            sh '''
+            #!/bin/bash
+            set -x
+            ls -l
+            echo "${imageId}"
+            '''
+    }
+
+}
