b2fefa9d682159289fd6ea7354d0cd03ceea2ea5
Sam Morrison <sorrison@gmail.com>
2020-10-02 09:06:05 +1000

    wip
    Change-Id: I8fa2e99de0ca2d6d9626faae34d9dbd34ac125e2

--- vars/deployTest.groovy
+++ vars/deployTest.groovy
@@ -7,12 +7,12 @@ def call(Map pipelineParams) {
     sh "echo $imageId"
     dir('build') {
         withCredentials([usernamePassword(credentialsId: '5c8f1b5c-2739-465e-ab10-e674b3fb884a', usernameVariable: 'OS_USERNAME', passwordVariable: 'OS_PASSWORD')]) {
-            sh '''
+            sh """
             #!/bin/bash
             set -x
             find .
             echo $imageId
-            '''
+            """
             sh "echo $imageId"
             
 
