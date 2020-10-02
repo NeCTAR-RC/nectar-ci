5b60b2136b83e999ed47bfb4a0ff80fb27abf4d3
Sam Morrison <sorrison@gmail.com>
2020-10-02 16:15:44 +1000

    wip
    Change-Id: I920a56d54154acf7bc32128838cc5706e55f60d3

--- vars/testOctaviaImage.groovy
+++ vars/testOctaviaImage.groovy
@@ -6,13 +6,13 @@ def call(String cloud_env, String availability_zone) {
     set +x
     echo "\033[33m========== Testing Image ==========\033[0m"
     . /opt/tempest/bin/activate
-    tmpdir=$(mktemp -d --suffix=_tempest)
+    tmpdir=\$(mktemp -d --suffix=_tempest)
     cd \$WORKSPACE/tempest
-    ./setup_tempest.py -s $availability_zone -e $cloud_env -j check-octavia-smoke $tmpdir
-    cd $tmpdir
+    ./setup_tempest.py -s $availability_zone -e $cloud_env -j check-octavia-smoke \$tmpdir
+    cd \$tmpdir
     stestr run --whitelist-file \$WORKSPACE/tempest/whitelists/check-octavia.yaml --serial 2>&1 | grep --line-buffered -vE ' \\w+Warning: |self._sock = None'
     RET=\${PIPESTATUS[0]}
-    rm -rf $tmpdir
-    exit $RET
+    rm -rf \$tmpdir
+    exit \$RET
     """
 }
