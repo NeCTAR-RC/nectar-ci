1656b02b3145376e0abc1dfa01e27c884b5108c5
Sam Morrison <sorrison@gmail.com>
2020-10-02 14:40:13 +1000

    wip
    Change-Id: I0150ba181970fcd1c0d5cbb28107cca28bb9d0c9

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -3,10 +3,10 @@ def call(String project_name, String cloud_env) {
     script {
         imageId = readFile(file: 'build/.image-id').trim()
         imageName = readFile(file: 'build/.facts/nectar_name').trim()
-        if $cloud_env == 'production' {
+        if ($cloud_env == 'production') {
             def os_cred_id = '7a2e4b77-a292-47a1-b852-c0cfd9c1c383'
             def os_auth_url = 'https://keystone.rc.nectar.org.au:5000/v3'
-        else {
+        } else {
             def os_cred_id = '5c8f1b5c-2739-465e-ab10-e674b3fb884a'
             def os_auth_url = 'https://keystone.test.rc.nectar.org.au:5000/v3'
         }
