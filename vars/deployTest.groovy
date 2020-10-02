c411fe626b52b2f1c1d5934b73e7002ff4c41577
Sam Morrison <sorrison@gmail.com>
2020-10-02 12:02:23 +1000

    wip
    Change-Id: Ibbec6b0c5eef29d3a708f05630826aea7786f2a1

--- vars/deployTest.groovy
+++ vars/deployTest.groovy
@@ -16,6 +16,7 @@ def call(String project_name) {
             export OS_PROJECT_NAME=$project_name
             [ -z "$imageName" ] && exit 1
             echo "Creating image $imageName..."
+            echo "SAMM"
             echo "==> openstack image create --id $imageId --disk-format qcow2 --container-format bare --file image.qcow2 'NeCTAR $imageName'"
             openstack image create -f value -c id --id $imageId --disk-format qcow2 --container-format bare --file image.qcow2 "NeCTAR $imageName" > image_id.txt
             echo "Image \$imageId created!"
