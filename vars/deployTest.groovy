603b2c313c3bf810532559f2007ac45fc9141c25
Sam Morrison <sorrison@gmail.com>
2020-10-02 10:52:07 +1000

    wip
    Change-Id: I7daa22f9987e24ae91e9394ed6ffc11e4bf7728e

--- vars/deployTest.groovy
+++ vars/deployTest.groovy
@@ -14,24 +14,21 @@ def call(String project_name) {
             export OS_USER_DOMAIN_NAME=Default
             export OS_IDENTITY_API_VERSION=3
             export OS_PROJECT_NAME=$project_name
-            env | grep OS_
-            IMAGE_ID="''' + "$imageId" + '''"
-            IMAGE_NAME="''' + "$imageName" + '''"
-            [ -z "\$IMAGE_NAME" ] && exit 1
-            echo "Creating image \$IMAGE_NAME..."
-            echo "==> openstack image create --id \$IMAGE_ID --disk-format qcow2 --container-format bare --file image.qcow2 \"NeCTAR \$IMAGE_NAME\""
-            #openstack image create -f value -c id --id \$IMAGE_ID --disk-format qcow2 --container-format bare --file image.qcow2 "NeCTAR \$IMAGE_NAME" > image_id.txt
-            echo "Image \$IMAGE_ID created!"
+            [ -z "$imageName" ] && exit 1
+            echo "Creating image $imageName..."
+            echo "==> openstack image create --id $imageId --disk-format qcow2 --container-format bare --file image.qcow2 \"NeCTAR $imageName\""
+            #openstack image create -f value -c id --id $imageId --disk-format qcow2 --container-format bare --file image.qcow2 "NeCTAR $imageName" > image_id.txt
+            echo "Image \$imageId created!"
             echo "Applying properties..."
             for FACT in .facts/*; do 
                PROP=\${FACT#*/}
                if ! echo "\$PROP" | grep -q '^nectar_'; then
                    VAL=`cat \$FACT`
                    echo " -> \$PROP: '\$VAL'..."
-                   #openstack image set --property \$PROP="\$VAL" \$IMAGE_ID
+                   #openstack image set --property \$PROP="\$VAL" $imageId
                fi
             done
-            #openstack image show --max-width=120 \$IMAGE_ID
+            #openstack image show --max-width=120 $imageId
             """
         }
     }
