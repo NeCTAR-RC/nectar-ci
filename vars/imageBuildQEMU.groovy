b3954d1e05e5c9a32da66f024133dd05d8a94485
Sam Morrison <sorrison@gmail.com>
2020-10-02 14:23:37 +1000

    Add some jenkins pipeline shared methods for image building
    Change-Id: I458a3a5ac340ad165a21d95c76ed5509b90ed48a

---
+++ vars/imageBuildQEMU.groovy
@@ -0,0 +1,22 @@
+def call(String imageName) {
+    git credentialsId: '4946c3a5-9f5e-4eac-9ec4-90e1e348db14', url: 'ssh://jenkins@review.rc.nectar.org.au:29418/NeCTAR-RC/nectar-images.git'
+    sh """#!/bin/bash
+        set +x
+        echo "\033[34m========== Building ==========\033[0m"
+        NAME=$imageName
+        OUTPUT_DIR=\$WORKSPACE/output-\$BUILD_TAG
+        rm -fr \$OUTPUT_DIR
+        rm -fr build
+        jq ".builders[0].name = \\"\$BUILD_TAG\\" | .builders[0].vm_name = \\"\$BUILD_TAG\\"" \$NAME.json > \$BUILD_TAG.json
+        echo "Starting packer build..."
+        packer build -color=true \$BUILD_TAG.json
+        echo "Shrinking image..."
+        echo "==> qemu-img convert -c -o compat=0.10 -O qcow2 \$BUILD_TAG \$BUILD_TAG.qcow2"
+        qemu-img convert -c -o compat=0.10 -O qcow2 \$OUTPUT_DIR/\$BUILD_TAG \$OUTPUT_DIR/image.qcow2
+        rm -rf \$OUTPUT_DIR/\$BUILD_TAG
+        mv \$OUTPUT_DIR build
+        echo `uuidgen` > build/.image-id
+        
+    """
+    stash includes: 'build/**', name: 'build'
+}
---
+++ vars/setImageTag.groovy
@@ -0,0 +1,27 @@
+def call(String project_name, String tag, String cloud_env) {
+    script {
+        imageId = readFile(file: 'build/.image-id').trim()
+        imageName = readFile(file: 'build/.facts/nectar_name').trim()
+        if $cloud_env == 'production' {
+            def os_cred_id = '6c8091b5-0e7d-4be5-8458-4e5a999acdd6'
+            def os_auth_url = 'https://keystone.rc.nectar.org.au:5000/v3'
+        else {
+            def os_cred_id = 'cc826c4e-07fe-4a0c-b334-fb8100b23c7b'
+            def os_auth_url = 'https://keystone.test.rc.nectar.org.au:5000/v3'
+        }
+    }
+    withCredentials([usernamePassword(credentialsId: $os_auth_url, usernameVariable: 'OS_USERNAME', passwordVariable: 'OS_PASSWORD')]) {
+       sh """
+       set +x
+       echo "\033[33m========== Promote to image in $cloud_env ==========\033[0m"
+       export OS_AUTH_URL=$os_auth_url
+       export OS_PROJECT_DOMAIN_NAME=Default
+       export OS_USER_DOMAIN_NAME=Default
+       export OS_IDENTITY_API_VERSION=3
+       export OS_PROJECT_NAME=$project_name
+       echo "Setting tag..."
+       echo "==> openstack image set --tag $tag $imageId"
+       openstack image set --tag $tag $imageId
+       """
+    }
+}
