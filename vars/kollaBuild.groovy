f84279d5ca850e653342c61db66faef3960c53b3
Sam Morrison <sorrison@gmail.com>
2021-11-09 09:01:33 +1100

    Tag and build with kolla build function
    Change-Id: I8f825c79082b11806db10851e1cab47da160c4c7

--- vars/kollaBuild.groovy
+++ vars/kollaBuild.groovy
@@ -1,4 +1,4 @@
-def call(String profile, String config_file) {
+def call(String profile, String tag, String config_file = "etc/kolla-build.conf") {
     sh """#!/bin/bash -eu
     export REGISTRY_AUTH_FILE=auth.json
     echo "\$REGISTRY_PSW" | docker login -u "\$REGISTRY_USR" --password-stdin "\$REGISTRY_HOST"
@@ -11,6 +11,12 @@ def call(String profile, String config_file) {
     docker pull \${REGISTRY_HOST}/kolla/ubuntu-source-base:\${OPENSTACK_RELEASE}
     docker pull \${REGISTRY_HOST}/kolla/ubuntu-source-openstack-base:\${OPENSTACK_RELEASE}
     kolla-build --config-file $config_file --profile $profile --skip-existing --tag \$OPENSTACK_RELEASE --registry \$REGISTRY_HOST
+
+    for image in `kolla-build --list-images --profile $profile --config-file $config_file | awk '{print \$3}' | egrep -v '^(base|openstack-base)\$'`
+    do
+      docker tag \${REGISTRY_HOST}/kolla/ubuntu-source-\${image}:\${OPENSTACK_RELEASE} \${REGISTRY_HOST}/kolla/ubuntu-source-\${image}:$tag
+      docker push \${REGISTRY_HOST}/kolla/ubuntu-source-\${image}:$tag
+    done
     """
 
 }
