baaf203d6ff861c39f55af98ff5aef1b2736e98f
Sam Morrison <sorrison@gmail.com>
2021-11-08 18:39:30 +1100

    Fix bash escaping for kollaBuild function
    Change-Id: If07947ffb1cb1afa375cfbf39c0ffb2eee880442

--- vars/kollaBuild.groovy
+++ vars/kollaBuild.groovy
@@ -1,16 +1,16 @@
 def call(String profile, String config_file) {
-    sh '''
+    """#!/bin/bash -eu
     export REGISTRY_AUTH_FILE=auth.json
-    echo "$REGISTRY_PSW" | docker login -u "$REGISTRY_USR" --password-stdin "$REGISTRY_HOST"
-    OPENSTACK_RELEASE=`echo $GERRIT_BRANCH | awk -F '/' '{ print $2 }'`
+    echo "\$REGISTRY_PSW" | docker login -u "\$REGISTRY_USR" --password-stdin "\$REGISTRY_HOST"
+    OPENSTACK_RELEASE=`echo \$GERRIT_BRANCH | awk -F '/' '{ print \$2 }'`
     virtualenv kolla
     . kolla/bin/activate
     pip install -r requirements.txt
     docker image prune --all -f
 
-    docker pull ${REGISTRY_HOST}/kolla/ubuntu-source-base:${OPENSTACK_RELEASE}
-    docker pull ${REGISTRY_HOST}/kolla/ubuntu-source-openstack-base:${OPENSTACK_RELEASE}
-    kolla-build --config-file $config_file --profile $profile --skip-existing --tag $OPENSTACK_RELEASE --registry $REGISTRY_HOST
-    '''
+    docker pull \${REGISTRY_HOST}/kolla/ubuntu-source-base:\${OPENSTACK_RELEASE}
+    docker pull \${REGISTRY_HOST}/kolla/ubuntu-source-openstack-base:\${OPENSTACK_RELEASE}
+    kolla-build --config-file $config_file --profile $profile --skip-existing --tag \$OPENSTACK_RELEASE --registry \$REGISTRY_HOST
+    """
 
 }
