3eebb7812bfa4addd149f3d786391486971980fd
Jacob Aharon <jacob.aharon@unimelb.edu.au>
2022-06-17 15:14:42 +1000

    Change the gerrit jenkins ssh key to ed25519
    Change-Id: I1104ee0a6bb6e498d4133c490f11561df70c313d

--- vars/testManilaImage.groovy
+++ vars/testManilaImage.groovy
@@ -1,6 +1,6 @@
 def call(String cloud_env, String availability_zone) {
 
-    git credentialsId: '4946c3a5-9f5e-4eac-9ec4-90e1e348db14', url: 'ssh://review.rc.nectar.org.au:29418/internal/nectar-testing.git'
+    git credentialsId: 'cd8b8dd3-b897-4ecb-985d-180d5b6f8498', url: 'ssh://review.rc.nectar.org.au:29418/internal/nectar-testing.git'
     sh """#!/bin/bash
     echo "\033[33m========== Testing Image ==========\033[0m"
     . /opt/tempest/bin/activate
