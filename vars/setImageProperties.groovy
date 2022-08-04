7d4036a3bd910030097af1425cb4c63d5ef69ba7
Andy Botting <andy@andybotting.com>
2022-08-04 21:07:30 +1000

    Apply all image properties with admin privs
    We had excluded applying nectar_* properties at the image deploy
    time as they're protected and needed admin. We were adding the
    nectar_name and nectar_build manually, but ignoring any others
    starting with nectar_. Moving all image properies to their own
    stage with admin privs means we can do the whole lot at once and
    it becomes a bit simpler
    
    Change-Id: Id447a0b53ece9b5dc3c2c981ba09bbb93b740b23

--- vars/setImageProperties.groovy
+++ vars/setImageProperties.groovy
@@ -1,7 +1,7 @@
 def call(String project_name, String cloud_env) {
+    unstash 'build'
     script {
         imageId = readFile(file: 'build/.image-id').trim()
-        imageName = readFile(file: 'build/.facts/nectar_name').trim()
         switch(cloud_env) {
           case "production":
             os_cred_id = '6c8091b5-0e7d-4be5-8458-4e5a999acdd6'
@@ -20,16 +20,21 @@ def call(String project_name, String cloud_env) {
 
     withCredentials([usernamePassword(credentialsId: os_cred_id, usernameVariable: 'OS_USERNAME', passwordVariable: 'OS_PASSWORD')]) {
        sh """#!/bin/bash
-       echo "\033[33m========== Set properties for $cloud_env ==========\033[0m"
-       export OS_AUTH_URL=$os_auth_url
-       export OS_PROJECT_DOMAIN_NAME=Default
-       export OS_USER_DOMAIN_NAME=Default
-       export OS_IDENTITY_API_VERSION=3
-       export OS_PROJECT_NAME=$project_name
-       echo "Setting nectar_name=$imageName"
-       openstack image set --property nectar_name="$imageName" $imageId
-       echo "Setting nectar_build=\$BUILD_NUMBER"
-       openstack image set --property nectar_build=\$BUILD_NUMBER $imageId
-       """
+        echo "\033[33m========== Set properties for $cloud_env ==========\033[0m"
+        export OS_AUTH_URL=$os_auth_url
+        export OS_PROJECT_DOMAIN_NAME=Default
+        export OS_USER_DOMAIN_NAME=Default
+        export OS_IDENTITY_API_VERSION=3
+        export OS_PROJECT_NAME=$project_name
+        echo "Applying properties..."
+        for FACT in build/.facts/*; do
+           PROP=\${FACT##*/}
+           VAL=`cat \$FACT`
+           echo " -> \$PROP: '\$VAL'"
+           openstack image set --property \$PROP="\$VAL" $imageId
+        done
+        echo "  -> nectar_build: '\$BUILD_NUMBER'"
+        openstack image set --property nectar_build=\$BUILD_NUMBER $imageId
+        """
     }
 }
