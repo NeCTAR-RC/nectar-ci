ae73c86a056ea299558c5ee45d4cc93d17a0a966
Andy Botting <andy@andybotting.com>
2024-08-30 21:31:50 +1000

    Fix non-escaped dollar sign
    Change-Id: I02018504af50846aacc2294b453711ee85020d80

--- vars/setImageID.groovy
+++ vars/setImageID.groovy
@@ -2,7 +2,7 @@ def call() {
     unstash 'build'
     sh """#!/bin/bash -eu
     echo "\033[34m========== Image ID ==========\033[0m"
-    ID=$(uuidgen)
+    ID=`uuidgen`
     echo "\$ID" > build/.image-id
     echo "New image ID is: \$ID"
     """
