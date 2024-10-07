6207599304597c392a6c90b1375e3453df15c8bb
Andy Botting <andy@andybotting.com>
2024-10-07 16:19:50 +1100

    Add some debugging for Jenkins slave build
    Change-Id: I4800fb092ea87ec96b7893f3a7c784930d972ee8

--- vars/imageBuildJenkins.groovy
+++ vars/imageBuildJenkins.groovy
@@ -1,6 +1,6 @@
 def call(String imageName) {
     git credentialsId: 'cd8b8dd3-b897-4ecb-985d-180d5b6f8498', url: 'ssh://jenkins@review.rc.nectar.org.au:29418/NeCTAR-RC/nectar-images.git'
-    sh """#!/bin/bash -eu
+    sh """#!/bin/bash -eux
         echo "\033[34m========== Building ==========\033[0m"
 
         IMAGE_NAME=$imageName
@@ -21,6 +21,10 @@ def call(String imageName) {
 
         # Move image build files to build directory
         mv \$OUTPUT_DIR build
+
+        # debug
+        find build
+
         mkdir -p raw_image
         mv build/image.qcow2 raw_image/
 
