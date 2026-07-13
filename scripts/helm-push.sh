51463d32a649f593af84c5b20f74d2c3074e2a9f
Sam Morrison <sorrison@gmail.com>
2026-07-13 16:23:12 +1000

    Updates for helm 4
    Change-Id: Ida3e07d3b86de5e059983af3e41089e1c9c590e7

--- scripts/helm-push.sh
+++ scripts/helm-push.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export HELM_EXPERIMENTAL_OCI=1
 export REGISTRY="registry.rc.nectar.org.au"
 echo "$REGISTRY_PSW" | helm registry login -u "$REGISTRY_USR" --password-stdin "$REGISTRY"
 find . -maxdepth 1 -name '*.tgz' -print0 | xargs -0 -I{} helm push {} oci://registry.rc.nectar.org.au/nectar-helm
